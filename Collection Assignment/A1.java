d4117c81edce4eba837875e6ecce621964785103
Sona2003 <102287696+Sona2003@users.noreply.github.com>
2023-10-28 10:58:02 +0530

    Add files via upload

---
+++ Collection Assignment/A1.java
@@ -0,0 +1,13 @@
+package com.collection;
+
+import java.util.HashMap;
+
+public class A1 
+{
+	public static void main(String[] args) 
+	{
+		//Initialize a HashMap
+		HashMap<String, String> hm = new HashMap<String, String>();
+		System.out.println(hm);
+	}
+}
---
+++ Collection Assignment/A2.java
@@ -0,0 +1,30 @@
+package com.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class A2
+{
+	public static void main(String[] args)
+	{
+		//Initialize a HashMap
+		HashMap<String, String> hm = new HashMap<String, String>();
+		
+		// Adding data in key & value pair
+		hm.put("Red", "Apple");
+		hm.put("Yellow", "Banana");
+		hm.put("Green", "Pair");
+		hm.put("Blue", "Berry");
+		
+		Set<Map.Entry<String, String>> s = hm.entrySet();
+		
+		for (Map.Entry<String, String> entry :s) 
+		{
+			String key = entry.getKey();
+			String val = entry.getValue();
+			System.out.println("Key: "+key+", Value: "+val);
+			
+		}
+	}
+}
